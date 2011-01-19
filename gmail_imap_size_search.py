4fe76c7273ce4aa1393a43ae43acca7b5824be0e
Pedro Pedruzzi <pedro.pedruzzi@gmail.com>
2011-01-18 22:40:32 -0200

    a lot of improvements. to summarize:  * tries to separate the functionality from the interactive cli  * uses proper password prompt  * sets default values  * imap server hostname and port are read from the user  * searches manually for big e-mails since it is a lot faster than imap search on google's servers  * decodes mailboxes names using imap's modified utf-7 standard  * decodes e-mail subjects using internationalized headers standard  * copies big e-mails to destination mailbox (currently hardcoded as BIGMAIL)

--- gmail_imap_size_search.py
+++ gmail_imap_size_search.py
@@ -13,45 +13,193 @@
 
 import imaplib
 import re
+import rfc822
+import StringIO
+import email.header
+import getpass
+import os
 
-# connecting to GMail through IMAP4 and SSL
-imap_connection=imaplib.IMAP4_SSL('imap.gmail.com', 993)
-# requesting your e-mail address and password
-username = raw_input('E-mail address: ')
-# note that password will be shown while you type it in terminal
-password = raw_input('Password: ')
-# requesting the size in MB
-size = raw_input("From what size you wanna find (in MB)? ")
-# converting the number typed into megabytes
-size = int(size) * 1024 * 1024
-print "\tSize: %s bytes." % (size)
-# connecting to GMail IMAP server
-imap_connection.login(username,password)
-#getting the list of the directories in your e-mail
-status, boxes = imap_connection.list()
-box_number = 0
-# printing the boxes in the screen
-for box in boxes:
-    box_number = box_number + 1
-    print "%d. %s" % (box_number, box)
-# requesting to select a directory
-selected = raw_input("Which box you wanna select? ")
-directory_name = re.search("(?<=\"/\"\s\")(.*)(?=\"$)", boxes[int(selected) - 1])
-print "\tSelected direcory: '%s'" % (directory_name.group(0))
-# selecting the directory in the server
-status, count = imap_connection.select(directory_name.group(0))
-print "\tYou have %s messages in the GMail '%s' directory." % (count[0], directory_name.group(0))
-# preparing the search of the large e-mails
-search_query = "(LARGER " + str(size) + ")"
-# searching the e-mails
-status, uids = imap_connection.search(None, search_query)
-uids = uids[0].split()
-regex = re.compile('(?<=(Subject:\s))(.*)')
-# printing the subject of the e-mails found in the screen
-for uid in uids:
-    status, data = imap_connection.fetch(uid, '(BODY[HEADER])')
-    mail_header = regex.search(data[0][1])
-    print ('\t Message #%s Subject: \'%s\'.') % (uid, mail_header.group(0).strip())
-# logging out of the server
-imap_connection.close()
-imap_connection.logout()
+
+
+# copied from http://docs.python.org/library/imaplib.html
+
+list_response_pattern = re.compile(r'\((?P<flags>.*?)\) "(?P<delimiter>.*)" (?P<name>.*)')
+
+def parse_list_response(line):
+    flags, delimiter, mailbox_name = list_response_pattern.match(line).groups()
+    mailbox_name = mailbox_name.strip('"')
+    return (flags, delimiter, mailbox_name)
+
+# FIXME: not sure if it works always. see: http://bugs.python.org/issue5305
+def decode_modified_utf7(s):
+    s = s.replace('&', '+')
+    ascii_mode = 1
+    for i in range(len(s)):
+        if ascii_mode:
+            if s[i] == '&':
+                ascii_mode = 0
+                s[i] = '+'
+        else:
+            if s[i] == ',':
+                s[i] = '/'
+            elif s[i] == '-':
+                ascii_mode = 1
+    # workaround for http://bugs.python.org/issue4425
+    s = s.replace('/', '+AC8-')
+    s = s.decode('utf7')
+    return s
+
+
+
+
+def test():
+    process('imap.gmail.com', 993, 'any@gmail.com', 'thing', 10 * 1024 * 1024, True)
+
+def input_or_default(prompt, default):
+    ret = raw_input("%s (default: %s): " % (prompt, default))
+    if len(ret) == 0:
+        ret = default
+    return ret
+
+def interative():
+    host = input_or_default('IMAP server hostname', 'imap.gmail.com')
+
+    port = int(input_or_default('IMAP server port', '993'))
+
+    username = raw_input('Login: ')
+
+    password = getpass.getpass('Password: ')
+
+    size = int(input_or_default('Minimum size in MB', '10'))
+
+    # convert to bytes
+    size = size * 1024 * 1024
+
+    process(host, port, username, password, size, True)
+
+
+
+def process(host, port, username, password, size, use_ssl=False):
+    # FIXME: make this a parameter
+    dest = 'BIGMAIL'
+
+    print "\t Connecting to %s:%d..." % (host, port)
+
+    # connect to IMAP server
+    if use_ssl:
+        imap_connection = imaplib.IMAP4_SSL(host, port)
+    else:
+        imap_connection = imaplib.IMAP4(host, port)
+
+    # authenticate by plain-text login
+    imap_connection.login(username, password)
+
+    # list and print mailboxes
+    status, boxes = imap_connection.list()
+
+    box = 1
+
+    # FIXME: this function should not be interactive
+    for ibox in range(len(boxes)):
+        # TODO: filter \Noselect flagged mailboxes
+        boxes[ibox] = parse_list_response(boxes[ibox])[2]
+
+        decoded = decode_modified_utf7(boxes[ibox])
+        if decoded == '[Gmail]/All Mail':
+            box = ibox + 1
+        print "%d. %s" % (ibox + 1, decoded)
+
+    # prompt for a mailbox
+    box = boxes[int(input_or_default("Mailbox", str(box))) - 1]
+
+    # select mailbox
+    status, data = imap_connection.select(box)
+    if status == 'NO':
+        print data
+
+    # print mailbox status
+    print "\tYou have %s messages in mailbox '%s'." % (data[0], decode_modified_utf7(box))
+
+    remsgsize = re.compile("(\d+) \(RFC822.SIZE (\d+).*\)")
+
+    msg_set = StringIO.StringIO()
+
+    print "\tLooking up big e-mails..."
+
+    status, data = imap_connection.fetch('1:*', '(RFC822.SIZE)')
+    count = 0
+    for msg in data:
+        match = remsgsize.match(msg)
+        msgid = int(match.group(1))
+        msgsize = int(match.group(2))
+
+        if msgsize >= size:
+            #print "to move: id=" + str(msgid) + ", size=" + str(msgsize)
+            msg_set.write(str(msgid))
+            msg_set.write(",")
+            count = count + 1
+
+    # remove trailing comma
+    msg_set.seek(-1, os.SEEK_CUR)
+    msg_set.truncate()
+
+    # StringIO -> str
+    msg_set = msg_set.getvalue()
+
+    #print msg_set
+    test_fetch_dump_subject(imap_connection, msg_set)
+
+    print "\tDone. %d e-mails found. Copying to mailbox '%s'..." % (count, dest)
+
+    # create destination mailbox, if new
+    status, data = imap_connection.create(dest)
+    if status == 'NO':
+	    pass
+        # we can ignore this failure assuming it is about a preexisting mailbox
+        # if it is not the case, than the copy will fail next
+
+    # copy to destination mailbox
+    status, data = imap_connection.copy(msg_set, dest)
+    if status == 'NO':
+        print data
+
+    # TODO: remove e-mails from original mailbox when it makes sense
+    # users generally want to _move_ big e-mails to separate mailboxes.
+    # however, some mail servers (like google's for instance) have a label/tag
+    # semantics for mailboxes thus making no point in removing a big e-mail
+    # from such a mailbox.
+
+    # close and sync selected mailbox
+    imap_connection.close()
+
+    # logout and close connection
+    imap_connection.logout()
+    imap_connection.shutdown()
+
+    print "\tDone!"
+
+
+
+
+def test_fetch_dump_subject(conn, message_set):
+    status, data = conn.fetch(message_set, '(BODY[HEADER.FIELDS (SUBJECT)])')
+    for piece in data:
+        if isinstance(piece, tuple):
+            test_dump_subject(piece[1])
+
+def test_dump_subject(header):
+    # workaround for http://bugs.python.org/issue504152
+    header = header.replace('\r\n ', ' ')
+    msg = rfc822.Message(StringIO.StringIO(header))
+    sub = msg["subject"]
+    data = email.header.decode_header(sub)
+    sub = data[0][0]
+    subcharset = data[0][1]
+    if subcharset != None:
+        sub = sub.decode(subcharset)
+    print '\tSubject: [%s].' % (sub)
+
+
+#test()
+
+interative()
